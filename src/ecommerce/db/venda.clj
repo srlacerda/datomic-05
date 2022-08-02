6429099771c1e4cb06237e701d502675f937a9e9
Diego Lacerda <diego.lacerda.alves@gmail.com>
2022-08-02 19:45:18 -0300

    utilizando atributos para representar situacoes no tempo

--- src/ecommerce/db/venda.clj
+++ src/ecommerce/db/venda.clj
@@ -10,7 +10,8 @@
     (d/transact conn [{:db/id            "venda"
                        :venda/produto    [:produto/id produto-id]
                        :venda/quantidade quantidade
-                       :venda/id         id}])
+                       :venda/id         id
+                       :venda/situacao   "nova"}])
     id))
 (defn instante-da-venda [db venda-id]
   (d/q '[:find ?instante .
@@ -32,21 +33,47 @@
            [(* ?preco ?quantidade) ?preco-por-produto]]
          (d/as-of db instante) venda-id)))
 
-(defn cancela! [conn venda-id]
-  (d/transact conn [[:db/retractEntity [:venda/id venda-id]]])
-  )
+
 
 (defn todas-nao-canceladas [db]
   (d/q '[:find ?id
-         :where [?venda :venda/id ?id]]
+         :where [?venda :venda/id ?id]
+         [?venda :venda/situacao ?situacao]
+         [(not= ?situacao "cancelada")]]
        db))
 
 (defn todas-inclusive-canceladas [db]
   (d/q '[:find ?id
-         :where [?venda :venda/id ?id _ true]]
-       (d/history db)))
+         :where [?venda :venda/id ?id]]
+       db))
 
 (defn canceladas [db]
   (d/q '[:find ?id
-         :where [?venda :venda/id ?id _ false]]
-       (d/history db)))
+         :where [?venda :venda/id ?id]
+         [?venda :venda/situacao "cancelada"]]
+        db))
+
+(defn altera-situacao! [conn venda-id situacao]
+  (d/transact conn [{:venda/id       venda-id
+                     :venda/situacao situacao}]))
+
+(defn cancela! [conn venda-id]
+  (altera-situacao! conn venda-id "cancelada"))
+
+(defn historico [db venda-id]
+  (->> (d/q '[:find ?instante ?situacao
+              :in $ ?id
+              :where [?venda :venda/id ?id]
+              [?venda :venda/situacao ?situacao ?tx true]
+              [?tx :db/txInstant ?instante]]
+            (d/history db) venda-id)
+       (sort-by first)))
+
+(defn historico-geral [db instante-desde]
+  (->> (d/q '[:find ?instante ?situacao ?id
+              :in $
+              :where [?venda :venda/id ?id]
+              [?venda :venda/situacao ?situacao ?tx]
+              [?tx :db/txInstant ?instante]]
+            (d/since db instante-desde))
+       (sort-by first)))
